376c59f55da83a4a1946417e1fc691a2c096bca9
tanish1243 <71543868+tanish1243@users.noreply.github.com>
2021-05-02 11:28:50 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+var firstname="Tanish";
+var secondname="Shetty"
+console.log("My first console project!")
+
+var fullname= firstname.concat(secondname);
+console.log(fullname)
+
+var firstnumber= 4 ;
+var secondnumber= 8 ;
+
+var add= firstnumber+secondnumber;
+console.log(add);
+
+var multiply= firstnumber*secondnumber;
+console.log(multiply);
+
+var divide= firstnumber/secondnumber;
+console.log(divide);
+
+var subtract=firstnumber-secondnumber;
+console.log(subtract);
